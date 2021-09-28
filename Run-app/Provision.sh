3096a87537a79ad892c7e64568fed263f785a33f
Cristian Mejia <crmejiam120@gmail.com>
2021-09-28 14:49:33 -0500

    Fixing provisioning, now both ui and api work

--- Run-app/Provision.sh
+++ Run-app/Provision.sh
@@ -3,19 +3,20 @@
 # First, we update apt-get in order to get the latest versions of all dependencies
 apt-get update -y
 
-# let's install nodeJS in order to be able to install package.json dependencies
-apt-get install nodejs -y
+# let's install npm and nodeJS in order to be able to install package.json dependencies
 apt-get install npm -y
 
 # Bypass https to be able to install app's dependencies without getting CERT_UNTRUSTED Error
 npm config set strict-ssl false
 
+# Let's install n helper to install the latest node stable version
+npm install -g n
+n stable 
+
 # Download the repo's contents
 apt-get install git -y
-git clone https://github.com/juan-ruiz/movie-analyst-api.git
-git clone https://github.com/juan-ruiz/movie-analyst-ui.git
-
-# Now, we're going to install all dependencies and build the app
+git clone https://github.com/crmejiam/movie-analyst-api.git
+git clone https://github.com/crmejiam/movie-analyst-ui.git
 
 # For Movie-Analyst-Api Repo
 cd movie-analyst-api
@@ -24,3 +25,10 @@ npm install -y
 # For Movie-Analyst-ui Repo
 cd ../movie-analyst-ui 
 npm install -y
+
+# Let's start running the ui and api to see if it works correctly (UI runs on port 3030 and api on port 3000)
+cd /home/vagrant/movie-analyst-ui
+npm start &              # start for movie-analyst-ui
+
+cd /home/vagrant/movie-analyst-api
+npm start &              # start for movie-analyst-api
