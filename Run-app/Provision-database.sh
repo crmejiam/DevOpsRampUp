aec81d4a100e77d7dd89aeae97d26e517f8b5515
Cristian Mejia <crmejiam120@gmail.com>
2021-09-30 11:56:56 -0500

    Fixing provisioning of database VM, now it creates the DB successfully

--- Run-app/Provision-database.sh
+++ Run-app/Provision-database.sh
@@ -4,14 +4,14 @@
 apt-get update -y
 
 # Let's install MySQL server
-apt-get install mysql-server -y
+echo 'mysql-server mysql-server/root_password password ubuntu' | debconf-set-selections
+echo 'mysql-server mysql-server/root_password_again password ubuntu' | debconf-set-selections
+apt install -y mysql-server
 
-# Now we need to create movie_db database
-mysql 
-CREATE DATABASE movie_db 
-
-# We need to execute the .sql file on movie-analyst-api repo
-source /home/vagrant/shared/table_creation_and_inserts.sql
-
-# [TEST] to see if DB was created correctly
-SELECT * FROM movie_db
+# Now we are going to log onto root user in MySQL, create the database movie_db, change the database
+# to movie_db and execute table_creation_and_inserts.sql file to create all data required
+mysql -u root -pubuntu << EOF
+CREATE DATABASE movie_db; 
+USE movie_db;
+source /home/vagrant/shared/table_creation_and_inserts.sql;
+EOF
