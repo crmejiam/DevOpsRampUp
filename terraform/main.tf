d6a2de8e5bb6ac2e6da471e66c86ee41c53080c4
Cristian Mejia <crmejiam120@gmail.com>
2021-10-21 16:44:42 -0500

    adding rds module to manage database with an rds instance instead of an ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,9 @@ provider "aws" {
 }
 
 module "ec2_instance" {
+
+  depends_on = [module.rds_instance]
+
   count               = length(var.server_type)
   server_type         = var.server_type[count.index]
   source              = "./modules/ec2"
@@ -28,6 +31,20 @@ module "ec2_instance" {
   volume_name         = var.volume_name[count.index]
   provisioner_file    = var.provisioner_file[count.index]
   port                = var.port[count.index]
+  ssh_port            = var.ssh_port
   privateIP           = var.privateIP[count.index]
   env_variables       = var.env_variables[count.index]
+  rds_endpoint         = module.rds_instance.rds_endpoint
+}
+
+module "rds_instance" {
+  source              = "./modules/rds"
+  InstanceType        = var.InstanceType
+  AvailabilityZone    = var.AvailabilityZone
+  rampup_subnet_id    = var.rampup_subnet_id[2]
+  trainee_tags        = var.trainee_tags
+  ramp_up_training_id = var.ramp_up_training_id
+  instance_name       = var.instance_name[2]
+  port                = var.port[2]
+  backendIP = var.privateIP[1]
 }
