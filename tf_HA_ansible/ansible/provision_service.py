b80db6ec65a7fc57eb672bfc860e8659415f91a6
Cristian Mejia <crmejiam120@gmail.com>
2021-11-19 12:23:28 -0500

    fixing public ssh key path

--- tf_HA_ansible/ansible/provision_service.py
+++ tf_HA_ansible/ansible/provision_service.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/register/<server_type>")
 def instance_provision(server_type):
     # Copy public ssh key to created instance
-    os.system('echo "yes\n" | ssh-copy-id -f -i /home/ubuntu/.ssh/id_rsa.pub -o "IdentityFile /home/ubuntu/{}" ubuntu@{}'.format("Ramp-Up-cristian.mejiam-Key-Pair.pem", request.args.get("ip")))
+    os.system('echo "yes\n" | ssh-copy-id -f -i /root/.ssh/id_rsa.pub -o "IdentityFile /home/ubuntu/{}" ubuntu@{}'.format("Ramp-Up-cristian.mejiam-Key-Pair.pem", request.args.get("ip")))
     # Call provision playbook
     os.system('ansible-playbook -i {}, /home/ubuntu/ansible/register.yml --extra-vars "server_type={}"'.format(request.args.get("ip"), server_type))
     return Response(status=200)
